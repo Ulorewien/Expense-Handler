09cffcf1ecb52bd096dbbe813f583b4db1fc3d74
Ulorewien <rohanshingre7@gmail.com>
2022-08-24 22:18:47 +0530

    Validation of date completed

--- dependencies/app.py
+++ dependencies/app.py
@@ -23,16 +23,29 @@ class App(threading.Thread):
         self.font_headers = self.specs["Application"]["font"] + " " + self.specs["Font_size"]["headers"]
         self.font_labels = self.specs["Application"]["font"] + " " + self.specs["Font_size"]["labels"]
         self.font_buttons = self.specs["Application"]["font"] + " " + self.specs["Font_size"]["buttons"]
+        self.font_inputs = self.specs["Application"]["font"] + " " + self.specs["Font_size"]["inputs"]
         self.Window = Tk()
         self.Window.title("Expense Handler")
         self.Window.configure(bg = self.bg_color)
         self.Window.resizable(False, False)
         self.Window.geometry(self.specs["Application"]["width"] + "x" + self.specs["Application"]["height"])
+        self.drawMenuBar()
         self.homePage()
         self.Window.mainloop()
 
+    def drawMenuBar(self):
+        self.menubar = Menu(self.Window)
+        self.options = Menu(self.menubar, tearoff = 0)
+        self.menubar.add_cascade(label = "Options", menu = self.options)
+        self.options.add_command(label = "Home", command = lambda:self.destroyPage(self.specs["Pages"]["home_page"]))
+        self.options.add_command(label = "Expense", command = lambda:self.destroyPage(self.specs["Pages"]["expense_page"]))
+        self.options.add_command(label = "Funds", command = lambda:self.destroyPage(self.specs["Pages"]["funds_page"]))
+        self.options.add_command(label = "Income", command = lambda:self.destroyPage(self.specs["Pages"]["income_page"]))
+        self.options.add_separator()
+        self.options.add_command(label = "Exit", command = self.Window.destroy)
+        self.Window.config(menu = self.menubar)
+
     def homePage(self):
-        print(self.font_headers)
         self.select_entry_label = Label(self.Window, text = "Select the type of entry", font = self.font_headers, background = self.bg_color)
         self.select_entry_label.place(relx = 0.5, rely = 0.15, anchor = "center")
         self.expense_button = Button(self.Window, text = "Expense", relief = RAISED, font = self.font_buttons, command = lambda:self.destroyPage(self.specs["Pages"]["expense_page"]))
@@ -67,7 +80,7 @@ class App(threading.Thread):
         self.expense_type_dropdown.place(relx = 0.45, rely = 0.4, relwidth= 0.2, anchor = "center")
         self.expense_amount_label = Label(self.Window, text = "Amount : ", font = self.font_labels, background = self.bg_color)
         self.expense_amount_label.place(relx = 0.2, rely = 0.55, anchor = "center")
-        self.expense_amount_entry = Entry(self.Window, font = self.font_labels, background = self.bg_color)
+        self.expense_amount_entry = Entry(self.Window, font = self.font_inputs, background = self.bg_color)
         self.expense_amount_entry.place(relx = 0.45, rely = 0.55, relwidth = 0.2, anchor = "center")
         self.mode_of_payment_label = Label(self.Window, text = "Payment method : ", font = self.font_labels, background = self.bg_color)
         self.mode_of_payment_label.place(relx = 0.2, rely = 0.7, anchor = "center")
@@ -75,7 +88,7 @@ class App(threading.Thread):
         self.mode_of_payment_dropdown = ttk.Combobox(self.Window, textvariable = self.mode_of_payment_variable, background = self.bg_color)
         self.mode_of_payment_dropdown["values"] = ["Gpay", "Cash"]
         self.mode_of_payment_dropdown.place(relx = 0.45, rely = 0.7, relwidth = 0.2, anchor = "center")
-        self.add_expense_button = Button(self.Window, text = "Add Expense", relief = RAISED, font = self.font_buttons, command = lambda:addExpense())
+        self.add_expense_button = Button(self.Window, text = "Add Expense", relief = RAISED, font = self.font_buttons, command = lambda:addExpense(self))
         self.add_expense_button.place(relx = 0.5, rely = 0.85, relwidth = 0.3, anchor = "center")
 
     def addFundsPage(self):
@@ -96,17 +109,17 @@ class App(threading.Thread):
         self.select_year_dropdown["values"] = getYearList()
         self.select_year_dropdown.place(relx = 0.85, rely = 0.25, relwidth = 0.15, anchor = "center")
         self.funds_amount_label = Label(self.Window, text = "Amount : ", font = self.font_labels, background = self.bg_color)
-        self.funds_amount_label.place(relx = 0.2, rely = 0.55, anchor = "center")
-        self.funds_amount_entry = Entry(self.Window, font = self.font_labels, background = self.bg_color)
-        self.funds_amount_entry.place(relx = 0.45, rely = 0.55, relwidth = 0.2, anchor = "center")
+        self.funds_amount_label.place(relx = 0.2, rely = 0.4, anchor = "center")
+        self.funds_amount_entry = Entry(self.Window, font = self.font_inputs, background = self.bg_color)
+        self.funds_amount_entry.place(relx = 0.45, rely = 0.4, relwidth = 0.2, anchor = "center")
         self.mode_of_payment_label = Label(self.Window, text = "Payment method : ", font = self.font_labels, background = self.bg_color)
-        self.mode_of_payment_label.place(relx = 0.2, rely = 0.7, anchor = "center")
+        self.mode_of_payment_label.place(relx = 0.2, rely = 0.55, anchor = "center")
         self.mode_of_payment_variable = StringVar()
         self.mode_of_payment_dropdown = ttk.Combobox(self.Window, textvariable = self.mode_of_payment_variable, background = self.bg_color)
         self.mode_of_payment_dropdown["values"] = ["Gpay", "Cash"]
-        self.mode_of_payment_dropdown.place(relx = 0.45, rely = 0.7, relwidth = 0.2, anchor = "center")
-        self.add_funds_button = Button(self.Window, text = "Add Funds", relief = RAISED, font = self.font_buttons, command = lambda:addFunds())
-        self.add_funds_button.place(relx = 0.5, rely = 0.85, relwidth = 0.3, anchor = "center")
+        self.mode_of_payment_dropdown.place(relx = 0.45, rely = 0.55, relwidth = 0.2, anchor = "center")
+        self.add_funds_button = Button(self.Window, text = "Add Funds", relief = RAISED, font = self.font_buttons, command = lambda:addFunds(self))
+        self.add_funds_button.place(relx = 0.5, rely = 0.7, relwidth = 0.3, anchor = "center")
 
     def addIncomePage(self):
         self.income_header_label = Label(self.Window, text = "Income", font = self.font_headers, background = self.bg_color)
@@ -133,15 +146,16 @@ class App(threading.Thread):
         self.income_type_dropdown.place(relx = 0.45, rely = 0.4, relwidth = 0.2, anchor = "center")
         self.income_amount_label = Label(self.Window, text = "Amount : ", font = self.font_labels, background = self.bg_color)
         self.income_amount_label.place(relx = 0.2, rely = 0.55, anchor = "center")
-        self.income_amount_entry = Entry(self.Window, font = self.font_labels, background = self.bg_color)
+        self.income_amount_entry = Entry(self.Window, font = self.font_inputs, background = self.bg_color)
         self.income_amount_entry.place(relx = 0.45, rely = 0.55, relwidth = 0.2, anchor = "center")
-        self.add_income_button = Button(self.Window, text = "Add Income", relief = RAISED, font = self.font_buttons, command = lambda:addIncome())
-        self.add_income_button.place(relx = 0.5, rely = 0.85, relwidth = 0.3, anchor = "center")
+        self.add_income_button = Button(self.Window, text = "Add Income", relief = RAISED, font = self.font_buttons, command = lambda:addIncome(self))
+        self.add_income_button.place(relx = 0.5, rely = 0.7, relwidth = 0.3, anchor = "center")
 
     def destroyPage(self, next_page_number):
         global page_number
         for widget in self.Window.winfo_children():
             widget.destroy()
+        self.drawMenuBar()
         if(next_page_number == self.specs["Pages"]["home_page"]):
             self.homePage()
         elif(next_page_number == self.specs["Pages"]["expense_page"]):
--- dependencies/date.py
+++ dependencies/date.py
@@ -17,3 +17,62 @@ def getYearList():
     current_year = date.today().year
     year_list = list(range(current_year, current_year - 11, -1))
     return year_list
+
+def validateDate(day, month, year):
+    day = day.replace(" ","")
+    month = month.replace(" ","")
+    year = year.replace(" ","")
+    if(day == "" or month == "" or year == ""):
+        return False
+
+    try:
+        day = int(day)
+        year = int(year)
+    except:
+        return False
+    
+    month_list = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    short_month_list = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"]
+    current_year = date.today().year
+    current_month = date.today().month
+    current_day = date.today().day
+    if(day < 1 or day > 31 or year > current_year or year < current_year - 10):
+        return False
+    if((month not in month_list) and (month not in short_month_list)):
+        return False
+    
+    try:
+        month_index = month_list.index(month) + 1
+    except:
+        month_index = short_month_list.index(month) + 1
+    if(year == current_year):
+        if(month_index > current_month):
+            return "Future Date"
+        elif(month_index == current_month):
+            if(day > current_day):
+                return "Future Date"
+
+    months_30 = ["April", "June", "September", "November"]
+    months_31 = ["January", "March", "May", "July", "August", "October", "December"]
+    short_months_30 = ["Apr", "Jun", "Sept", "Nov"]
+    short_months_31 = ["Jan", "Mar", "May", "Jul", "Aug", "Oct", "Dec"]
+
+    if((month in months_30) or (month in short_months_30)):
+        if(day <= 30):
+            return True
+        else:
+            return False
+
+    elif(month == "Feb" or month == "February"):
+        if(year%4 == 0):
+            if(day <= 29):
+                return True
+            else:
+                return False
+        else:
+            if(day <= 28):
+                return True
+            else:
+                return False
+    
+    return True
